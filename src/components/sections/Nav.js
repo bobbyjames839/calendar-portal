d53a678083a419dfe56ed8ba38c1aa64d5db1bed
Bobby Davidson <bobbyjames839@gmail.com>
2024-09-15 23:43:26 +0100

    update

--- src/components/sections/Nav.js
+++ src/components/sections/Nav.js
@@ -4,7 +4,6 @@ import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
 
 export const Nav = () => {
-
     const navigate = useNavigate();
 
     return (
